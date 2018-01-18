3e5949e40f7d0722c4884e54ea39093cb8ae587e
Thor Erik Lie <thor@thorerik.com>
2018-01-18 13:30:41 +0100

    remove custom color class for hsla->rgb conversion

--- src/Lib/Maths.ts
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @param {number} min  Floor for the random number
- * @param {number} max  Ceiling for the random number
- * @return {number}     Random within min and max
- */
-export function randomFloat(min: number, max: number) {
-    return Math.random() * (max - min) + min;
-}
