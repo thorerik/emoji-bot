479dd307d11b0b96679d4783b33085877492e617
Thor Erik Lie <thor@thorerik.com>
2018-01-21 15:13:05 +0100

    include debug info for a bug

--- dist/Events/CommandProcessor.js
+++ dist/Events/CommandProcessor.js
@@ -7,27 +7,38 @@ class CommandProcessor extends EventBase_1.EventBase {
         super();
     }
     async run(message) {
-        const guildConfiguration = await GuildConfiguration_1.GuildConfiguration.findOne({ where: { guildID: message.guild.id.toString() } });
-        const guildConfig = JSON.parse(guildConfiguration.settings);
-        if (!message.content.startsWith(guildConfig.prefix)) {
+        this.message = message;
+        if (this.message.guild === null) {
             return;
         }
-        const args = message.content.split(/\s+/g);
-        let command = args.shift().toLowerCase();
+        const guildConfiguration = await GuildConfiguration_1.GuildConfiguration.findOne({ where: { guildID: message.guild.id.toString() } });
+        if (guildConfiguration.settings === null) {
+            throw new Error(`Settings is null for ${message.guild.id}`);
+        }
+        const guildConfig = JSON.parse(guildConfiguration.settings);
+        if (!this.message.content.startsWith(guildConfig.prefix)) {
+            return;
+        }
+        this.args = this.message.content.split(/\s+/g);
+        let command = this.args.shift().toLowerCase();
         command = command.split(guildConfig.prefix)[1];
+        this.runCommand(command);
+    }
+    runCommand(command) {
         try {
             const cmd = this.props.getCommand(command);
             if (
             // User has permission
-            (typeof cmd.permissionRequired !== "string" && message.member.hasPermission(cmd.permissionRequired)) ||
+            (typeof cmd.permissionRequired !== "string" &&
+                this.message.member.hasPermission(cmd.permissionRequired)) ||
                 // User is owner
                 (typeof cmd.permissionRequired === "string" &&
                     cmd.permissionRequired === "BOT_OWNER" &&
-                    this.props.config.config.owners.includes(message.member.id))) {
-                cmd.run(message, args);
+                    this.props.config.config.owners.includes(this.message.member.id))) {
+                cmd.run(this.message, this.args);
             }
             else {
-                message.reply(`Sorry, you do not have permission for this command`);
+                this.message.reply(`Sorry, you do not have permission for this command`);
             }
         }
         catch (e) {
--- src/Events/CommandProcessor.ts
+++ src/Events/CommandProcessor.ts
@@ -5,33 +5,45 @@ import { GuildConfiguration } from "../Database/Models/GuildConfiguration";
 import { EventBase } from "../Lib/EventBase";
 
 export class CommandProcessor extends EventBase {
+    private args: string[];
+    private message: Message;
     constructor() {
         super();
     }
     public async run(message: Message) {
+        this.message = message;
+
+        if (this.message.guild === null) {
+            return;
+        }
         const guildConfiguration = await GuildConfiguration.findOne({where: {guildID: message.guild.id.toString()}});
+        if (guildConfiguration.settings === null) {
+            throw new Error(`Settings is null for ${message.guild.id}`);
+        }
         const guildConfig = JSON.parse(guildConfiguration.settings);
 
-        if (!message.content.startsWith(guildConfig.prefix)) { return; }
+        if (!this.message.content.startsWith(guildConfig.prefix)) { return; }
 
-        const args = message.content.split(/\s+/g);
-        let command = args.shift().toLowerCase();
+        this.args = this.message.content.split(/\s+/g);
+        let command = this.args.shift().toLowerCase();
         command = command.split(guildConfig.prefix)[1];
+        this.runCommand(command);
+    }
+
+    private runCommand(command: string) {
         try {
             const cmd = this.props.getCommand(command);
             if (
                 // User has permission
-                (typeof cmd.permissionRequired !== "string" && message.member.hasPermission(cmd.permissionRequired)) ||
+                (typeof cmd.permissionRequired !== "string" &&
+                this.message.member.hasPermission(cmd.permissionRequired)) ||
                 // User is owner
-                (
-                    typeof cmd.permissionRequired === "string" &&
+                (typeof cmd.permissionRequired === "string" &&
                     cmd.permissionRequired === "BOT_OWNER" &&
-                    this.props.config.config.owners.includes(message.member.id)
-                )
-            ) {
-                cmd.run(message, args);
+                    this.props.config.config.owners.includes(this.message.member.id))) {
+                cmd.run(this.message, this.args);
             } else {
-                message.reply(`Sorry, you do not have permission for this command`);
+                this.message.reply(`Sorry, you do not have permission for this command`);
             }
         } catch (e) {
             // We don't really care for errors here
