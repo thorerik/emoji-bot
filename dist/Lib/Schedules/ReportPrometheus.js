5c673195fd9a5c86dea5aa2c8c04bfbeb7fd4a13
Thor Erik Lie <thor@thorerik.com>
2018-02-01 13:04:13 +0100

    fix issue with prom reporting

--- dist/Lib/Schedules/ReportPrometheus.js
+++ dist/Lib/Schedules/ReportPrometheus.js
@@ -1,19 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const log = require("fancy-log");
 const schedule = require("node-schedule");
 const snekfetch = require("snekfetch");
 const Properties_1 = require("../Properties");
 class ReportPrometheus {
     static async run() {
-        return schedule.scheduleJob("*/1 * * * *", () => {
+        return schedule.scheduleJob("*/1 * * * *", async () => {
             const props = Properties_1.Properties.getInstance();
             const data = `bot_messages ${props.messages}
-            bot_guilds ${props.client.guilds.size}
-            bot_channels ${props.client.channels.size}`;
+bot_guilds ${props.client.guilds.size}
+bot_channels ${props.client.channels.size}
+`;
             props.messages = 0;
-            snekfetch.put(`${props.config.config.pushGateway}/metrics/job/bots/instance/emojibot`, {
-                data,
-            });
+            try {
+                await snekfetch.put(`${props.config.config.pushGateway}/metrics/jobs/bots/instances/emojibot`, {
+                    data,
+                });
+            }
+            catch (e) {
+                log.error(e);
+            }
         });
     }
 }
--- src/Lib/Schedules/ReportPrometheus.ts
+++ src/Lib/Schedules/ReportPrometheus.ts
@@ -1,3 +1,4 @@
+import * as log from "fancy-log";
 import * as schedule from "node-schedule";
 import * as snekfetch from "snekfetch";
 
@@ -5,15 +6,20 @@ import { Properties } from "../Properties";
 
 export class ReportPrometheus {
     public static async run() {
-        return schedule.scheduleJob("*/1 * * * *", () => {
+        return schedule.scheduleJob("*/1 * * * *", async () => {
             const props = Properties.getInstance();
             const data = `bot_messages ${props.messages}
-            bot_guilds ${props.client.guilds.size}
-            bot_channels ${props.client.channels.size}`;
+bot_guilds ${props.client.guilds.size}
+bot_channels ${props.client.channels.size}
+`;
             props.messages = 0;
-            snekfetch.put(`${props.config.config.pushGateway}/metrics/job/bots/instance/emojibot`, {
-                data,
-            });
+            try {
+                await snekfetch.put(`${props.config.config.pushGateway}/metrics/jobs/bots/instances/emojibot`, {
+                    data,
+                });
+            } catch(e) {
+                log.error(e);
+            }
         });
     }
 }
